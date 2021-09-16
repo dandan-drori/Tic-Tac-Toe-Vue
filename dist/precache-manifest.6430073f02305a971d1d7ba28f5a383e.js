dcbf1d8b67ac60130c9e48ada369d0a0d9fe7a4b
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2021-09-16 21:48:26 +0300

    add dist

---
+++ dist/precache-manifest.6430073f02305a971d1d7ba28f5a383e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f904faa3add8b4896eab",
+    "url": "/Tic-Tac-Toe-Vue/css/app.352be7b4.css"
+  },
+  {
+    "revision": "8109c5a765a193deb1cda4c84376eee6",
+    "url": "/Tic-Tac-Toe-Vue/index.html"
+  },
+  {
+    "revision": "f904faa3add8b4896eab",
+    "url": "/Tic-Tac-Toe-Vue/js/app.3c0eafb7.js"
+  },
+  {
+    "revision": "db3f0c1a38ce2ae2ac5a",
+    "url": "/Tic-Tac-Toe-Vue/js/chunk-a495e78a.99fe1a63.js"
+  },
+  {
+    "revision": "26f49c65ea887e54c7f9",
+    "url": "/Tic-Tac-Toe-Vue/js/chunk-a4c53104.434277e0.js"
+  },
+  {
+    "revision": "f5de538cd444057944a8",
+    "url": "/Tic-Tac-Toe-Vue/js/chunk-b5b6c666.5a93a684.js"
+  },
+  {
+    "revision": "da3efbb69bce8d92b9e9",
+    "url": "/Tic-Tac-Toe-Vue/js/chunk-vendors.27bc1eef.js"
+  },
+  {
+    "revision": "49ef5d73b56ab519e736ff31343a786b",
+    "url": "/Tic-Tac-Toe-Vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Tic-Tac-Toe-Vue/robots.txt"
+  }
+]);
